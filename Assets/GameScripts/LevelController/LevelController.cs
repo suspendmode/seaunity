adadaa4439836b9cb800db5aad3df0fce4773395
zhiwilliam <zhiwilliam@gmail.com>
2012-08-17 12:34:30 +0000

    Upload For testing

---
+++ Assets/GameScripts/LevelController/LevelController.cs
@@ -0,0 +1,37 @@
+//============================================================================================================
+// Weili Zhi Copy right reserved.
+//============================================================================================================
+/// Action Base
+//============================================================================================================
+// Created on 18/7/2012 9:06:52 AM by Weili Zhi
+//============================================================================================================
+using UnityEngine;
+using LitJson;
+using System.Collections;
+
+public class LevelController : AIControllerBase
+{
+	public GameObject PlayerShip;
+	private LevelData mLevelData;
+    void Awake()
+	{
+		// Add Data and Actions.
+		string path = Application.dataPath + "/" + Settings.LevelDataPath;
+		mLevelData = JsonExtend.Load<LevelData>(path);
+		
+	}
+	void Start()
+	{
+		gameObject.SendMessage(mLevelData.WelcomeEvent);
+	}
+	
+	void TryStrategyMode()
+	{
+		PlayerShip.SetActiveRecursively(false);
+	}
+	
+	void TryExplorationMode()
+	{
+		PlayerShip.SetActiveRecursively(true);
+	}
+}
